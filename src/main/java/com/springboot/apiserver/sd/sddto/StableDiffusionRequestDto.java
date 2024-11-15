5142a772b5a075f453b1e3cd5859e12d47891812
Jun Young <aa2030822@naver.com>
2024-11-15 15:52:36 +0900

    ✨ AWS 배포환경 연결

--- src/main/java/com/springboot/apiserver/sd/sddto/StableDiffusionRequestDto.java
+++ src/main/java/com/springboot/apiserver/sd/sddto/StableDiffusionRequestDto.java
@@ -1,15 +1,13 @@
 package com.springboot.apiserver.sd.sddto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class StableDiffusionRequestDto {
+    private int id;
     private String prompt;
     private String initImage;
 }
